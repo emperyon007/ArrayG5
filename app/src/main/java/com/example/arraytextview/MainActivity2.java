00115a7763822a4070174557653c4799c07679b0
emperyon007 <lorenzopapini232@gmail.com>
2021-12-14 09:50:13 +0100

    Added Toast and listener

--- app/src/main/java/com/example/arraytextview/MainActivity2.java
+++ app/src/main/java/com/example/arraytextview/MainActivity2.java
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -22,6 +25,18 @@ public class MainActivity2 extends AppCompatActivity {
         LsStati = (ListView) findViewById(R.id.listaStati);
         ArrayAdapter<String> aaStati = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, nazioni);
         LsStati.setAdapter(aaStati);
+
+        LsStati.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int pos, long id) {
+                String stato = (String)LsStati.getItemAtPosition(pos);
+                Log.d("errori: ", "poszione:" + pos);
+
+                int duration = Toast.LENGTH_SHORT;
+                Toast popStat = Toast.makeText(getApplicationContext(), stato, duration);
+                popStat.show();
+            }
+        });
 
     }
 }
